ff1fa84fb2df7c243345044cb43c9f07698c4217
wolfstons <paczi.balazs@gmail.com>
2026-09-25 14:16:01 +0200

    v1

--- library/routes/api.php
+++ library/routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/users', [UserConroller::class, 'index']);
+Route::get('/users/{user}', [UserConroller::class, 'show']);
