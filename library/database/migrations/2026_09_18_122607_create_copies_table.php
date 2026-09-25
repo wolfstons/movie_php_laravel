cf9217cb1eb6fd6e5b6582fd649888647d6ac967
wolfstons <paczi.balazs@gmail.com>
2026-09-25 15:46:47 +0200

    v..5

--- library/database/migrations/2026_09_18_122607_create_copies_table.php
+++ library/database/migrations/2026_09_18_122607_create_copies_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('copies', function (Blueprint $table) {
             $table->id();
             $table->foreignId('book_id')->constrained();
-            $table->foreignId('user_id')->constrained();
+            $table->boolean("hardcover")->default(false);
+            $table->date("publication")->nullable();
+            $table->status("available")->default(true);
             $table->timestamps();
         });
     }
